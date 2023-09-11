04d7bcc4bf9ec3938a06b3dd18a3ad0102a693c1
John Sison <jlsison777@gmail.com>
2023-09-11 00:21:48 -0400

    add event listener for numbers

--- calculator.js
+++ calculator.js
@@ -1,9 +1,19 @@
+// -----------------queries---------------------
 let previousValue = document.querySelector('.previous-value');
-let currentValue = document.querySelector('current-value');
-let number = document.querySelectorAll('.number');
+let currentValue = document.querySelector('.current-value');
+let numbers = document.querySelectorAll('.numbers');
 let operator = document.querySelectorAll('.operator');
 let clear = document.querySelector('.clear');
 let del = document.querySelector('.del');
 let decimal = document.querySelector('.decimal');
 let equals = document.querySelector('.equals');
+// ---------------------------------------
+let currentValueContent = ''
+
+numbers.forEach(num => {
+  num.addEventListener('click', e => {
+    currentValueContent += e.target.textContent; 
+    currentValue.textContent = currentValueContent
+  })
+})
 
