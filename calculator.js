301d5dc46a4cc1d7c5316f23821e9fefaa4ca71e
John Sison <jlsison777@gmail.com>
2023-09-10 23:33:01 -0400

    made queries for all buttons in javascript

---
+++ calculator.js
@@ -0,0 +1,9 @@
+let previousValue = document.querySelector('.previous-value');
+let currentValue = document.querySelector('current-value');
+let number = document.querySelectorAll('.number');
+let operator = document.querySelectorAll('.operator');
+let clear = document.querySelector('.clear');
+let del = document.querySelector('.del');
+let decimal = document.querySelector('.decimal');
+let equals = document.querySelector('.equals');
+
